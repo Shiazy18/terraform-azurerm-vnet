fdf3502efa1cc5b64bf2ddeeeb878fea809877c0
Shivam <shivampant0998@gmail.com>
2025-08-07 11:50:00 +0530

    added examples

--- example/with_rg/main.tf
+++ example/with_rg/main.tf
@@ -2,6 +2,6 @@ module vnet {
     source = "../.."
     rg_purpose = "vnet"
     location = "eastus"
-    address_space = [""]   
+    address_space = ["10.0.1.0/24"]   
     vnet_purpose = "app"
 }
--- example/without_rg/main.tf
+++ example/without_rg/main.tf
@@ -3,4 +3,5 @@ module vnet {
     resource_group_name = "demo-vnet"
     location = "eastus"
     address_space = ["10.0.0.6/24"]
+    vnet_purpose = "app"
 }
