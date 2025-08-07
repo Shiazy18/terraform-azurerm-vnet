508469544565876e05b90ea4257e44d058877d81
Shivam <shivampant0998@gmail.com>
2025-08-07 11:51:50 +0530

    fmt and tags to be added later

--- example/without_rg/main.tf
+++ example/without_rg/main.tf
@@ -1,7 +1,7 @@
-module vnet {
-    source = "../.."
-    resource_group_name = "demo-vnet"
-    location = "eastus"
-    address_space = ["10.0.0.6/24"]
-    vnet_purpose = "app"
+module "vnet" {
+  source              = "../.."
+  resource_group_name = "demo-vnet"
+  location            = "eastus"
+  address_space       = ["10.0.0.6/24"]
+  vnet_purpose        = "app"
 }
--- variables.tf
+++ variables.tf
@@ -1,38 +1,38 @@
 variable "resource_group_name" {
-    type = string
-    description = "Name of existing resource group (if rg_purpose is empty)"
-    default = ""
+  type        = string
+  description = "Name of existing resource group (if rg_purpose is empty)"
+  default     = ""
 }
 
 variable "location" {
-    type = string
-    description = "location for your resource" 
-    validation {
+  type        = string
+  description = "location for your resource"
+  validation {
     condition     = contains(["East US", "West US", "Central US"], var.location)
     error_message = "Location must be one of the following: 'East US', 'West US', 'Central US'."
   }
 }
 
 variable "rg_purpose" {
-    type = string
-    description = "Purpose for the resource group. Leave empty to use an existing RG."
-    default = ""
-  
+  type        = string
+  description = "Purpose for the resource group. Leave empty to use an existing RG."
+  default     = ""
+
 }
 
 variable "vnet_purpose" {
-    type = string
-    description = "Vnet Purpose"
-    default = ""
+  type        = string
+  description = "Vnet Purpose"
+  default     = ""
 }
 
 variable "virtual_network_name" {
-    type = string
-    description = "Vnet Purpose"
-    default = ""
+  type        = string
+  description = "Vnet Purpose"
+  default     = ""
 }
 
 variable "address_space" {
-  type = string
+  type        = string
   description = "address space for vnet"
 }
