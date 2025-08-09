8b0c310a680834a0cf2f61fb55057f4dbb88de90
Shivam <shivampant0998@gmail.com>
2025-08-09 15:39:05 +0530

    minor changes to module and modified gitignore

--- variables.tf
+++ variables.tf
@@ -4,12 +4,18 @@ variable "resource_group_name" {
   default     = ""
 }
 
+variable "environment" {
+  type        = string
+  description = "Enviroment tag"
+  default     = ""
+}
+
 variable "location" {
   type        = string
   description = "location for your resource"
   validation {
     condition     = contains(["East US", "West US", "Central US"], var.location)
-    error_message = "Location must be one of the following: 'East US', 'West US', 'Central US'."
+    error_message = "Location must be one of the following: 'East US', 'West US', 'c'."
   }
 }
 
@@ -33,6 +39,6 @@ variable "virtual_network_name" {
 }
 
 variable "address_space" {
-  type        = string
+  type        = list(string)
   description = "address space for vnet"
 }
