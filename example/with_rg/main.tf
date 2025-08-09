e244da4b253f2cb113c797f4a89e036d07150afc
Shivam <shivampant0998@gmail.com>
2025-08-09 15:39:47 +0530

    minor changes to module and modified gitignore

--- example/with_rg/main.tf
+++ example/with_rg/main.tf
@@ -1,7 +1,9 @@
 module "vnet" {
-  source        = "../.."
+  # source        = "Shiazy18/vnet/azurerm"
+  source = "../.."
   rg_purpose    = "vnet"
-  location      = "eastus"
+  location      = "East US"
   address_space = ["10.0.1.0/24"]
   vnet_purpose  = "app"
+  environment = "test"
 }
--- locals.tf
+++ locals.tf
@@ -1,4 +1,16 @@
 locals {
-  rg_name   = var.rg_purpose == "" ? azurerm_resource_group.rg[0].name : var.resource_group_name
-  vnet_name = var.virtual_network_name == "" ? "${var.vnet_purpose}-${var.location}-vnet" : var.virtual_network_name
+  tags = {
+    environment = var.environment
+    location = var.location
+  }
+  rg_name   = var.rg_purpose != "" ? azurerm_resource_group.rg[0].name : var.resource_group_name
+  vnet_name = var.virtual_network_name == "" ? "${var.vnet_purpose}-${local.rg_location}-${var.environment}-vnet" : var.virtual_network_name
+
+  location_map = {
+    "East US" = "eastus"
+    "West US" = "westus"
+    "Central US" = "centralus"
+  }
+
+  rg_location = local.location_map[var.location]
 }
